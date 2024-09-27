31b37f6fe50924395aea5f6d4a945e6824c0bae4
Al Stone <astone@heckgrammar.co.uk>
2024-09-27 14:13:47 +0100

    Added some data types

--- DataTypes12D24/Program.cs
+++ DataTypes12D24/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // datatype identifier (optional assignment)
+            // integer
+            int myInteger;
+            myInteger = 42;
+            Console.WriteLine(myInteger);
+
+            //real/float
+            float myFloat = 0.579F;
+            decimal myDecimal = 1.342946M;
+            double myDouble = 1.23456;
+
+            //Boolean
+            bool myBool = true;
+            myBool = false;
+
+            //character
+            char myChar = 'P';
+
+            //string
+            string myString = "This is a string";
+
+
         }
     }
 }
