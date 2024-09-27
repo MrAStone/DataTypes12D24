06332b71c3d50949d7ea6aff06e7f04df5ad601f
Al Stone <astone@heckgrammar.co.uk>
2024-09-27 13:49:14 +0100

    Add project files.

---
+++ DataTypes12D24/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DataTypes12D24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
